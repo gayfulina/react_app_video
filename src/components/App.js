f7814b5b139e6a77ffde7050b3209a075f36a3d6
gayfulina <irinmdr@gmail.com>
2020-11-16 14:55:13 +1000

    app

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,22 @@ import youtube from '../apis/youtube';
 const KEY = "YOUR_KEY";
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        console.log(term);
+    state = {videos: []};
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+        this.setState({videos: response.data.items});
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                I have {this.state.videos.length}
             </div>
         )
     }
