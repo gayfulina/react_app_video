f02d264abf8b67b609c2cf8f7c9596b5c9ca076e
gayfulina <irinmdr@gmail.com>
2020-11-16 15:00:12 +1000

    video list

---
+++ src/components/VideoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class VideoItem extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default VideoItem;
